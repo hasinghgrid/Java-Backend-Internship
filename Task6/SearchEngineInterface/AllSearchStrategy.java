2c8bbbfa2af2a48ab7b0311b54706fff54ead379
hasinghgrid <hasingh@griddynamics.com>
2025-02-05 15:28:19 +0530

    Add files via upload

---
+++ Task6/SearchEngineInterface/AllSearchStrategy.java
@@ -0,0 +1,18 @@
+package search.SerachEngineInterface;
+
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class AllSearchStrategy implements SimpleSearchEngine {
+
+    @Override
+    public Set<Integer> search(String[] queryWords, Map<String, Set<Integer>> invertedIndex) {
+        Set<Integer> resultIndexes = new HashSet<>(invertedIndex.getOrDefault(queryWords[0], Set.of()));
+        for (int i = 1; i < queryWords.length; i++) {
+            resultIndexes.retainAll(invertedIndex.getOrDefault(queryWords[i], Set.of()));
+        }
+        return resultIndexes;
+    }
+}
