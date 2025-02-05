b06b9f387a202edc1ead54f485c68c2e830122c5
hasinghgrid <hasingh@griddynamics.com>
2025-02-05 15:15:05 +0530

    Create Main.java

---
+++ Task2/Main.java
@@ -0,0 +1,11 @@
+package search;
+
+import search.SimpleSearchEngine.Search;
+
+class Main{
+    public static void main(String[] args) {
+        Search s1 = new Search();
+        s1.getInput();
+    }
+}
+
